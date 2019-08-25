a1b41a3d198916351862fab0a50fad82efd17a2a
翔 <297962650@qq.com>
2019-08-25 20:57:21 +0800

    完成登录页面组件结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入包含所有less文件的index
+import '@/styles/index.less'
+import router from '@/router/router.js'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
